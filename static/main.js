abeb07797464dacdfd309efb87422f7930110091
Racloody <gageotelouan@pm.me>
2024-09-15 19:36:40 +0200

    hi han hinnnn

--- static/main.js
+++ static/main.js
@@ -1,19 +1,3 @@
-
-async function getData() {
-    const url = "/oembed?url=" + encodeURI("https://open.spotify.com/playlist/5xgYeWxF4lKIeqOsSjfwXt?si=7ae1153ef3b64040");
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-      const json = await response.json();
-      console.log(json.html);
-      const div = document.getElementById('oembed');
-      div.innerHTML = json.html;
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-
-getData()
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+    //
+  };
